7efce9e41ad05a0906971bd663874eaf06047763
Milli-saved <million12.tenkir@gmail.com>
2022-11-15 14:28:29 +0300

    get route is setup.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const { get } = require("mongoose");
 
 const app = express();
 
+app.get("/", (req, res, next) => {
+  console.log("first get function.");
+});
 
 const port = 4000;
 app.listen(4000, () => {
